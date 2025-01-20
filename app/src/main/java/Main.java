01df4313e8a6058a1555c051f9410a01c6bf2edc
Yuki SUEYOSHI <e245741@ie.u-ryukyu.ac.jp>
2025-01-20 19:40:19 +0900

    2nd commit

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,26 +1,43 @@
+import java.util.Arrays;
+import java.util.List;
+
 import jp.ac.uryukyu.ie.e245741.*;
 
 public class Main {
 
-public static void main(String[] args) {
-    Deck deck = new Deck();
+    public static void main(String[] args) {
+        // デッキを作成してシャッフル
+        Deck deck = new Deck();
         deck.shuffle();
+
+        // プレイヤーを作成
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        List<Player> players = Arrays.asList(player1, player2);
+
+        // カードを配布
+        while (deck.getCards().size() > 0) {
+            player1.receiveCards(deck.dealCards(1));
+            if (deck.getCards().size() > 0) {
+                player2.receiveCards(deck.dealCards(1));
+            }
+        }
+
+        // テーブルを初期化
         Table table = new Table();
 
-        // プレイヤーを作成し、カードを配る
-        Player player = new Player("Alice");
-        player.receiveCards(deck.dealCards(52));
+        // 最初のプレイヤーを決定してゲーム開始
+        GameMaster gameMaster = new GameMaster();
+        gameMaster.decideFirstPlayer(player1,"Spades", 7 );
+        gameMaster.decideFirstPlayer(player2,"Spades", 7);
 
-        // 現在の手札を表示
-        //System.out.println(player);
+        gameMaster.placeAllSevens(players, table);
+        
+        gameMaster.playTurn(player2, table);
+        table.displayTable();
+        
 
-        // カードを場に出す
-        player.playCard("Spades", 7, table);
-
-        // 現在の場の状態と手札を表示
-        System.out.println("Table: " + table.getCardsBySuit("Spades"));
-        //System.out.println(player);
-}
+    }
 }
 
     
